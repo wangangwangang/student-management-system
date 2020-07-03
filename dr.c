7b0fa0404b36486d166c9b968dea6b9d9747b52f
yisheng <1911847374@qq.com>
2020-07-03 15:09:21 +0800

    qqq

--- dr.c
+++ dr.c
@@ -22,27 +22,28 @@ int DRStu()
 	{
 		printf("打开文件失败。");
 		printf("按回车键继续\n");
-			setbuf(stdin,NULL);
-			getchar();
+			setbuf(stdin,NULL);				//清空键盘缓冲区
+			getchar();						//作用是吸收一个字符，这里是用来控制时间
 		return 0;
 	}
 
 	while(1)
 	{
 
-		pstu=(PSTU)malloc(sizeof(STU));//在堆内存申请空间，储存学生信息
-		pstu->next=NULL;//申请新的链表节点时，一定要将指针区赋值为空
+		pstu=(PSTU)malloc(sizeof(STU));			//在堆内存申请空间，储存学生信息
+		pstu->next=NULL;						//申请新的链表节点时，一定要将指针区赋值为空
 
 		if(!pstu)
 		{
 			printf("申请内存空间失败。\n");
 			return 0;
 		}
+
 		if(fscanf(pa,"%d %s %s %d %d %d %d %d %d ",&pstu->num,pstu->name,pstu->code,&pstu->age,&pstu->class,&pstu->sx,&pstu->yw,&pstu->cyy,&pstu->mc)!=EOF)
 		{
-			while(ps->next)//找到当前链表的最后一个节点的指针    
+			while(ps->next)						//找到当前链表的最后一个节点的指针    
 				ps=ps->next;
-			ps->next=pstu;//把新节点加入到最后那个节点的后面
+			ps->next=pstu;						//把新节点加入到最后那个节点的后面
 		}
 		else 
 		{
